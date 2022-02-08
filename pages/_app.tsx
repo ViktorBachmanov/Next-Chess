c5d0e4e9ecc708ec50539ff7640ccc91969eb57c
Vikt0r <vbachmanov@mail.ru>
2022-02-08 10:09:33 +0300

    Apply MuiTheme on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0';
 
+import createMainTheme, { LightStatus } from '../lib/muiTheme';
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const mainTheme = createMainTheme(LightStatus.DARK);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </UserProvider>
   )
 }
