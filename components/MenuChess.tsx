5dbb9edc9d7a53b7280decda2ce7f2dffbfe2283
Vikt0r <vbachmanov@mail.ru>
2022-02-25 18:08:55 +0300

    Add logout menu item

--- components/MenuChess.tsx
+++ components/MenuChess.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
-import { useUser } from '@auth0/nextjs-auth0';
+import { useUser } from "@auth0/nextjs-auth0";
 
-import GameDialog from './GameDialog'
-
+import GameDialog from "./GameDialog";
 
 export default function MenuChess() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -22,9 +21,8 @@ export default function MenuChess() {
   const { user, error, isLoading } = useUser();
 
   const [isDialogOpen, setDialogOpen] = useState(false);
-  const [dialogTitle, setDialogTitle] = useState('');
-  const [dialogSubmitLabel, setDialogSubmitLabel] = useState('');
-
+  const [dialogTitle, setDialogTitle] = useState("");
+  const [dialogSubmitLabel, setDialogSubmitLabel] = useState("");
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -34,24 +32,27 @@ export default function MenuChess() {
   };
 
   function handleLogin() {
-    router.push('/api/auth/login');
+    router.push("/api/auth/login");
+  }
+
+  function handleLogout() {
+    router.push("/api/auth/logout");
   }
 
   function openDialogAddGame() {
-    setDialogTitle('Новая партия');
-    setDialogSubmitLabel('Добавить');
+    setDialogTitle("Новая партия");
+    setDialogSubmitLabel("Добавить");
     setDialogOpen(true);
     handleClose();
   }
 
   function openDialogDeleteGame() {
-    setDialogTitle('Последняя партия');
-    setDialogSubmitLabel('Удалить');
+    setDialogTitle("Последняя партия");
+    setDialogSubmitLabel("Удалить");
     setDialogOpen(true);
     handleClose();
   }
 
-  
   return (
     <div>
       {/*<Button
@@ -64,18 +65,18 @@ export default function MenuChess() {
         Dashboard
       </Button>*/}
 
-      {console.log('MenuChess return')}
+      {console.log("MenuChess return")}
 
       <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={handleClick}
-          >
-            <MenuIcon />
-        </IconButton>
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </IconButton>
 
       <Menu
         id="demo-positioned-menu"
@@ -84,35 +85,29 @@ export default function MenuChess() {
         open={open}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
+          vertical: "bottom",
+          horizontal: "left",
         }}
         transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
+          vertical: "top",
+          horizontal: "left",
         }}
       >
-        <MenuItem 
-            onClick={handleLogin}
-            disabled={user !== undefined}
-        >
-            Войти
+        <MenuItem onClick={handleLogin} disabled={user !== undefined}>
+          Войти
         </MenuItem>
-        <MenuItem 
-            onClick={openDialogAddGame}
-            disabled={user === undefined}
-        >
-            Добавить партию
+        <MenuItem onClick={handleLogout} disabled={user === undefined}>
+          Выйти
         </MenuItem>
-        <MenuItem 
-            onClick={openDialogDeleteGame}
-            disabled={user === undefined}
-        >
-            Удалить последнюю
+        <MenuItem onClick={openDialogAddGame} disabled={user === undefined}>
+          Добавить партию
+        </MenuItem>
+        <MenuItem onClick={openDialogDeleteGame} disabled={user === undefined}>
+          Удалить последнюю
         </MenuItem>
       </Menu>
 
-      <GameDialog 
+      <GameDialog
         isDialogOpen={isDialogOpen}
         setDialogOpen={setDialogOpen}
         title={dialogTitle}
