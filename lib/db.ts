81a29710961dbf1e449a4dd4a613141b88a9ff11
Vikt0r <vbachmanov@mail.ru>
2025-05-31 13:51:26 +0300

    add MYSQL_USER env variable

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ export const db = mysql({
   config: {
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
   },
 });
